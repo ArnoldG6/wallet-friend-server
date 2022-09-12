bcd08d1dfb2cf148024a510c98d89747bf2a5543
ArnoldG6 <arnoldgq612@gmail.com>
2022-09-11 21:35:52 -0600

    Testing alternative CORS settings

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from wallet_friend_services import AuthService
 HTTP server config.
 """
 app = Flask(__name__)
-CORS(app)  # Enabling CORS on whole app, if it is necessary, this should be changed for every path.
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})  # Enabling CORS on whole app, if it is necessary, this should be changed for every path.
 secret_key = secrets.token_urlsafe(256)  # setting secret  with newly generated secret SHA256 key.
 app.secret_key = secret_key
 api_versions = ["v1.0"]

b7e1bc0634bc547dd0d5afa2703eff71cb7b129e
ArnoldG6 <arnoldgq612@gmail.com>
2022-09-11 22:33:48 -0600

    CORS settings re-applied

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ HTTP server config.
 """
 app = Flask(__name__)
 CORS(app)  # Enabling CORS on whole app, if it is necessary, this should be changed for every path.
-app.config['CORS_HEADERS'] = 'Content-Type'
 secret_key = secrets.token_urlsafe(256)  # setting secret  with newly generated secret SHA256 key.
 app.secret_key = secret_key
 api_versions = ["v1.0"]
@@ -33,29 +32,29 @@ latest_version = api_versions[-1]
 @app.route(f"/{latest_version}/api/users/authenticate", methods=["POST"])
 def users_authenticate():
     try:
-        return AuthService(request).auth_user_service(secret_key), 200, {"Access-Control-Allow-Origin": "*"}
+        return AuthService(request).auth_user_service(secret_key), 200
     except HttpWalletFriendException as e:
         logging.error(e)
-        return e.json(), e.get_code(), {"Access-Control-Allow-Origin": "*"}
+        return e.json(), e.get_code(),
 
     except BaseException as e:
         logging.error(e)
         e = InternalServerException()  # Exception overwrite to protect server's logs.
-        return e.json(), e.get_code(), {"Access-Control-Allow-Origin": "*"}
+        return e.json(), e.get_code()
 
 
 @app.route(f"/{latest_version}/api/users/register", methods=["POST"])
 def users_register():
     try:
-        return AuthService(request).register_user_service(), 200,  {"Access-Control-Allow-Origin": "*"}
+        return AuthService(request).register_user_service(), 200
     except HttpWalletFriendException as e:
         logging.error(e)
-        return e.json(), e.get_code(), {"Access-Control-Allow-Origin": "*"}
+        return e.json(), e.get_code()
 
     except BaseException as e:
         logging.error(e)
         e = InternalServerException()  # Exception overwrite to protect server's logs.
-        return e.json(), e.get_code(),  {"Access-Control-Allow-Origin": "*"}
+        return e.json(), e.get_code()
 
 
 """

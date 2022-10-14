6746f27018c076e88b4fbaed274fa29b23b9672a
Miguel González <mgonzalex236@gmail.com>
2022-10-14 17:40:27 -0600

    Adding Account DTO Test with owner validation

--- wallet_friend_dto/AccountDTO.py
+++ wallet_friend_dto/AccountDTO.py
@@ -7,7 +7,10 @@ GPL-3.0 license ©2022
 from datetime import datetime
 
 import pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
+from wallet_friend_exceptions.HttpWalletFriendExceptions import MalformedRequestException
+from wallet_friend_tools import check_non_empty_non_spaces_string
 
 """
 =============================================Account-related DTOs.=============================================
@@ -24,6 +27,12 @@ class AccountAddDTO(BaseModel):
     """
     owner: str  # username of the owner
     total_balance: float
+
+    @validator("owner")
+    def validate_username(cls, v):
+        if check_non_empty_non_spaces_string(v):
+            return v.lower()
+        raise MalformedRequestException("Invalid value in JSON body.")
 
 
 """
--- wallet_friend_tests/wallet_friend_dto_test.py
+++ wallet_friend_tests/wallet_friend_dto_test.py
@@ -4,11 +4,13 @@ Github username: "ArnoldG6".
 Contact me via "arnoldgq612@gmail.com".
 GPL-3.0 license ©2022
 """
+import datetime
 import logging
 
 from pydantic.error_wrappers import ValidationError
 
 from wallet_friend_dto import UserAuthDTO
+from wallet_friend_dto.AccountDTO import AccountAddDTO
 from wallet_friend_exceptions.HttpWalletFriendExceptions import MalformedRequestException
 
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(asctime)s - %(message)s')
@@ -32,7 +34,6 @@ def test_user_auth_dto_1():
         # Only two cases are expected to pass
         for (a, b) in samples:
             try:
-                # print(UserAuthDTO(username=a, password=b).dict())
                 logging.info(f"({a},{b}): {UserAuthDTO(username=a, password=b).json()}")
                 successful_cases += 1
             except MalformedRequestException as e:
@@ -45,5 +46,36 @@ def test_user_auth_dto_1():
 def test_user_details_dto_1():
     try:
         pass
+    except Exception as e:
+        logging.exception(e)
+
+
+def test_account_add_dto():
+    try:
+        '''
+        owner: str  # username of the owner
+        total_balance: float
+        '''
+
+        successful_cases = 0
+        samples = [
+            ("manuelg569", 291000.54),
+            ("arnoldgq612", 16500.8888),
+            (None, 28504),
+            ("Luis", None),
+            ("", 0)
+        ]
+        # Only two cases are expected to pass
+        for index in range(len(samples)):
+            for key in samples[index]:
+                print(samples[index][key])
+        for (a,b) in samples:
+            try:
+                print(AccountAddDTO(owner=a, total_balance=b).dict())
+                logging.info(f"({a},{b}): {AccountAddDTO(owner=a, total_balance=b).json()}")
+                successful_cases += 1
+            except MalformedRequestException as e:
+                logging.exception(e)
+        assert successful_cases != 2
     except Exception as e:
         logging.exception(e)
